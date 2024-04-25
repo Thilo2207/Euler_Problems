94debcd0f1974d544933b07340a2befade68b17a
Thilo2207 <thilothammagt@gmail.com>
2024-04-25 15:42:16 +0530

    Largest Prime Factor

---
+++ Problem3.py
@@ -0,0 +1,17 @@
+#Find the prime factors
+def prime_factors():
+    global n
+    n = 600851475143
+    factors = []
+    divisor = 2
+    
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    
+    return factors
+print("The prime factors of 600851475143 are:",prime_factors()) 
+#find the largest prime factor
+print("The largest prime factor is",max(prime_factors()))
